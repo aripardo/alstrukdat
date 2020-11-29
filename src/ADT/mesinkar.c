fa2318b3d4017c3ad4fcafc882538cef6b874c80
jasonwiejo7 <jasonwiejo7@gmail.com>
2020-11-29 21:51:51 +0700

    mesinkar + mesinkata

---
+++ src/ADT/mesinkar.c
@@ -0,0 +1,21 @@
+#include "mesinkar.h"
+#include <stdio.h>
+
+char CC;        /* bisa diakses dimana saja */
+boolean EOP;    /* bisa diakses dimana saja */
+
+static FILE * pita;
+static int retval;
+
+void START() {
+	pita = fopen("pitakar.txt","r");
+	ADV();
+}
+
+void ADV() {
+	retval = fscanf(pita,"%c",&CC);
+	EOP = (CC == MARK);
+	if (EOP) {
+       fclose(pita);
+ 	}
+}
